134a13990e86fc5b3642217282082e9269f8df14
Vladi <vladi@notifyai.io>
2023-01-31 17:37:28 +0200

    working, but need style fixes

--- src/app/features/pokemon/pokemon.module.ts
+++ src/app/features/pokemon/pokemon.module.ts
@@ -1,19 +1,33 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-import { PokemonRoutingModule } from './pokemon-routing.module';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { NgModule } from '@angular/core';
 import { PokemonComponent } from './pokemon.component';
-import { FormComponent } from './components/form/form.component';
-
+import { PokemonRoutingModule } from './pokemon-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormComponent } from "./components/form/form.component";
+import { ListComponent } from './components/list/list.component';
+import { MatCardModule } from "@angular/material/card";
+import { DetailComponent } from './components/detail/detail.component';
 
 @NgModule({
   declarations: [
     PokemonComponent,
-    FormComponent
+    FormComponent,
+    ListComponent,
+    DetailComponent
   ],
   imports: [
     CommonModule,
-    PokemonRoutingModule
-  ]
+    PokemonRoutingModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    MatDialogModule,
+  ],
 })
-export class PokemonModule { }
+export class PokemonModule {}
